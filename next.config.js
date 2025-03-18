f3bf3cf47abe2a1e26d836ebda606d11e6f94f3a
mark pancake <mark@sky-cloud.net>
2025-03-18 13:40:19 +0800

    config  commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-}
-
-module.exports = nextConfig
+    output: 'export',
+    images: {unoptimized: true},
+  }
+  
+  module.exports = nextConfig
